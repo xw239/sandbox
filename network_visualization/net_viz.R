db82b678c0b86909f299f42bc46f74b08b3a6499
Xun Wang <xun.wang@sentrana.com>
2016-03-20 21:11:05 -0400

    Added first attempt of D3 visualization to the Sioux Falls Network. Now downloads files automatically from the internet

--- network_visualization/net_viz.R
+++ network_visualization/net_viz.R
@@ -3,27 +3,41 @@
 ## Some analysis adopted from http://kateto.net/network-visualization
 
 #set a temp working directory.To be removed after switching to 
-setwd('C:/Users/xwang/Documents/GitHub/sandbox/network_visualization/data/Transportation_Networks/')
+#setwd('C:/Users/xwang/Documents/GitHub/sandbox/network_visualization/data/Transportation_Networks/')
 
 library(igraph)
 library(data.table)
-library(RColorBrewer)
-library(dplyr)#display.brewer.all()
+library(RColorBrewer) #display.brewer.all()
+library(dplyr)
 library(networkD3)
-library(animation)
 
 # Network 1: Sioux Fall (Illustrative)
-#Read in the Sioux Falls Network
-Nodes <- data.table(read.delim2('SiouxFalls_node.txt', header = T))
-Edges <- data.table(read.delim2('SiouxFalls_net.txt', header = T, skip=7))
-Flows <- data.table(read.delim2('SiouxFalls_flow.txt', header = T, dec = '.'))
+#download and load the Sioux Falls Network
+download.file('http://www.bgu.ac.il/~bargera/tntp/SiouxFalls/SiouxFalls_node.zip',
+              'SiouxFalls_node.zip')
+download.file('http://www.bgu.ac.il/~bargera/tntp/SiouxFalls/SiouxFalls_net.zip',
+              'SiouxFalls_net.zip')
+download.file('http://www.bgu.ac.il/~bargera/tntp/SiouxFalls/SiouxFalls_flow.zip',
+              'SiouxFalls_flow.zip')
+
+# To download files, make sure to have write access on your working directory
+Nodes <- data.table(read.delim2(unz('SiouxFalls_node.zip','SiouxFalls_node.txt'), 
+                                header = T))
+Edges <- data.table(read.delim2(unz('SiouxFalls_net.zip','SiouxFalls_net.txt'), 
+                                header = T, skip=7))
+Flows <- data.table(read.delim2(unz('SiouxFalls_flow.zip','SiouxFalls_flow.txt'),
+                                header = T, dec = '.'))
+
+#delete downloaded files
+unlink(c('SiouxFalls_node.zip', 'SiouxFalls_net.zip', 'SiouxFalls_flow.zip'))
+
 #clean up exccessive columns
 Nodes <- Nodes[, .(Node, X,Y)]
 Edges <- Edges[, 2:(ncol(Edges)-1), with =F]
 
-Edges <- merge(Edges, Flows[, .(From, To, Volume)], by.x = c('Init.node', 'Term.node'),
+Edges <- merge(Edges, Flows[, .(From, To, Volume)], 
+               by.x = c('Init.node', 'Term.node'),
                by.y = c('From', 'To'))  
-
 rm(Flows)
 #create the graphics
 net <- graph.data.frame(Edges, vertices = Nodes, directed = T)
@@ -55,25 +69,39 @@ edge_weights <- scale(E(net)$Volume, center =F)*6
 vertex_degree <- degree(net)
 colors <- brewer.pal(length(unique(vertex_degree)), "Set3")
 
-#use the average of inflow and outflow as vertex size
+#use the average of total inflow and outflow for each vertex as vertex size
 out_flow <- Edges[,.(volume_out = sum(Volume)), by = .(node=Init.node)]
 in_flow <- Edges[,.(volume_in = sum(Volume)), by = .(node = Term.node)]
 avg_flow <- unlist(merge(out_flow,in_flow, all = T,
                             by = 'node')[, .(weight = (volume_out+volume_in)/2)])
-vertex_size <- (avg_flow +2*min(avg_flow))/3000 #add minimums to reduce range 
 
-plot(net, edge.arrow.size = 0.7, edge.color = 'darkgray', 
+vertex_size <- (avg_flow +2*min(avg_flow))/3500 #add minimums to reduce range 
+
+#adjust edge color to illustrate a shortest path
+#returns the indices of selected edeges
+hl_path <- unlist(shortest_paths(net, from = V(net)[24], to = V(net)[7], 
+                 mode = 'all', weight = E(net)$Free.Flow.Time, 
+                 output = 'epath' )$epath)
+
+#adjust color based on whether the edge belongs to the path
+edge_color <- ifelse(1:length(E(net)) %in% hl_path, 'gold', 'darkgray')
+
+plot(net, edge.arrow.size = 0.7, edge.color = edge_color, 
      vertex.size = vertex_size,
      vertex.color =  colors[dense_rank(vertex_degree)],
-     edge.width = edge_weights, edge.curved = 0,
-     rescale =F,
-     layout = layout.norm(cbind(X= V(net)$X, Y= V(net)$Y))*1.2)
+     edge.width = edge_weights, edge.curved = 0, #edge.label = round(E(net)$Volume),
+     rescale =F, layout = layout.norm(cbind(X= V(net)$X, Y= V(net)$Y))*1.1, 
+     mark.groups=c(1,4,5,8), mark.col="#C5E5E7", mark.border=NA) # mark an area
 
-#display.brewer.pal(length(unique(vertex_degree)), "Set3")
+#make a D3 forcednetwork. Currently difficult to set nodes to fixed postition
+
+#prepare node sets and edge sets
+Edges_D3 <- Edges[, .(from = Init.node-1, to = Term.node-1, Value = Free.Flow.Time)]
+Nodes_D3 <- Nodes[, .(ID = Node-1, degree = vertex_degree/2)]
+
+forceNetwork(Edges_D3, Nodes_D3, Source = 'from', Target = 'to', Value = 'Value',
+             NodeID = 'ID', Group = 'degree', legend =T, width =  500, height = 500,
+             zoom = T,  opacity = 0.9)
 
 # Network 2: Chicago Regional (Stress Testing)
 #net.m <- net - E(net)[E(net)$type=="hyperlink"]
-#plot(net, mark.groups=c(1,4,5,8), mark.col="#C5E5E7", mark.border=NA)
-#news.path <- get.shortest.paths(net, V(net)[media=="MSNBC"],
- #                               V(net)[media=="New York Post"],
-  #                              mode="all", output="both")
